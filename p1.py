2328f2efe638b023bd6f2e4f1c63ba7e55a00fe3
Santiago Benavides <santiago.benavides@alumnos.udg.mx>
2022-02-24 17:11:16 -0600

    First step Done

--- p1.py
+++ p1.py
@@ -4,17 +4,17 @@ from vacuum import Vacuum
 def create_enviroment():
 
     room_id = []
-    room_status = []
+    rooms_status = []
 
     #Creación de habitaciones
     while(True):
-        rooms = input("Introduce el número de habitaciones (2 a 6): ")
-        if rooms.isdigit() and int(rooms) >= 2 and int(rooms) <= 6:
+        rooms = input("Introduce el número de habitaciones (2 a 26): ")
+        if rooms.isdigit() and int(rooms) >= 2 and int(rooms) <= 26:
             rooms = int(rooms)
             vacuum = ["" for i in range(rooms)]
             break
         else:
-            print("Error, introduce un número entre 2 y 6")
+            print("Error, introduce un número entre 2 y 26")
     a = 65
     i = 0
 
@@ -22,11 +22,12 @@ def create_enviroment():
     while(i < rooms):
         status = input(f"Introduce el estado de la habitación {chr(a)} (0 = limpia, 1 = sucia): ")
         if status.isdigit() and int(status) >= 0 and int(status) <= 1:
+            # Room status es una lista
+            # True si la habitación está limpia y False si está sucia
             if int(status) == 0:
-                room_status.append((True,False))
+                rooms_status.append(True)
             else:
-                #Room status es
-                room_status.append((False,False))
+                rooms_status.append(False)
             room_id.append(chr(a))
             a += 1
             i += 1
@@ -37,13 +38,13 @@ def create_enviroment():
     while(True):
         pos = input(f"Introduce la posición de la aspiradora ({room_id}): ")
         if pos.isalpha() and pos.upper() in room_id:
-            vacuum[room_id.index(pos.upper())] = "_-_/|"
+            vacuum = Vacuum(rooms_status, int(ord(pos.upper())-65))
             break
         else:
             print("Error, introduce una habitación válida")
 
     #Representación de la aspiradora
     print("Configuración de la habitación:\n")
-    print(tabulate.tabulate([room_id, vacuum, room_status], tablefmt='grid'))
+    vacuum.draw_vacuum()
 
 create_enviroment()
--- vacuum.py
+++ vacuum.py
@@ -1,7 +1,44 @@
+from tabulate import tabulate
+from copy import deepcopy
+
 class Vacuum:
     rooms_checked = []
+    # move = 1, check = 2, clean = 3
     cost = 0
 
-    def __init__(rooms_state,vacuum_pos):
-        self.rooms_state = rooms_state
+    def __init__(self, rooms_status, vacuum_pos):
+        self.rooms_status = rooms_status
         self.vacuum_pos = vacuum_pos
+
+    def check_all(self):
+        vacuum_solve = deepcopy(self)
+        # Revisa su posición inicial y se mueve a la derecha
+        # Revisando y limpiando en caso de ser necesario
+        last_pos = self.vacuum_pos
+        for i in range(vacuum_solve.vacuum_pos, len(vacuum_solve.rooms_status)):
+            if i not in vacuum_solve.rooms_checked:
+                if vacuum_solve.rooms_status[i] == False:
+                    #Aspiramos
+                    vacuum_solve.rooms_status[i] = True
+                    vacuum_solve.cost += 3
+                #Si está limpia, solo se agrega a la lista de habitaciones revisadas
+                vacuum_solve.rooms_checked.append(i)
+                vacuum_solve.cost += 2
+            #Si ya está revisada, solo se mueve
+            vacuum_solve.cost += 1
+            last_pos = i
+        vacuum_solve.vacuum_pos = last_pos
+        vacuum_solve.cost -= 1
+        print(f"Original (Coste -> {self.cost}):")
+        self.draw_vacuum()
+        print(f"\nSolución (Coste -> {vacuum_solve.cost}):")
+        vacuum_solve.draw_vacuum()
+
+    def draw_vacuum(self):
+        vacuum = [ "" for i in range(len(self.rooms_status)) ]
+        vacuum[self.vacuum_pos] = "_-_/|"
+        dirty = [ "_____" if room else "XXXXX" for room in self.rooms_status ]
+        print(tabulate([[chr(i+65) for i in range(len(self.rooms_status))], vacuum, dirty], tablefmt='grid'))
+
+vacuum = Vacuum([True,False], 0)
+vacuum.check_all()
