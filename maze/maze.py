6eadf1fe94c22a57614b449b9eb0c3ac57c6eb42
Santiago Benavides <santiago.benavides@alumnos.udg.mx>
2022-04-04 12:17:45 -0500

    DFS and IDS

--- maze/maze.py
+++ maze/maze.py
@@ -7,7 +7,7 @@ import time
 
 COUNTER = [0,0]
 COORDINATES = [None,None]
-IMAGE_NAME = "big.png"
+IMAGE_NAME = "maze.png"
 X,Y = 0,0
 
 class Node:
@@ -23,7 +23,7 @@ class Node:
         return self.x == other.x and self.y == other.y
 
 #Solving the maze with bfs
-def solve():
+def bfs():
     start = time.time()
     global X
     global Y
@@ -58,6 +58,106 @@ def solve():
         current = current.parent
     path.reverse()
     os.system("cls")
+    print("Path:")
+    for node in path:
+        print(node,end=" ")
+    print()
+    for node in path:
+        plt.plot(node.x,node.y,'b.', markersize=4)
+    fig.canvas.draw()
+    print("Nodes expanded:",len(explored))
+    print("Depth:",len(path))
+    print("Execution time:",time.time()-start)
+
+#Solving the maze with deep first search
+def dfs():
+    start = time.time()
+    global X
+    global Y
+    maze = {}
+    image = Image.open(IMAGE_NAME)
+    image = image.convert('RGB')
+    X,Y = image.size[0]-1,image.size[1]-1
+    for y in range(image.height):
+        for x in range(image.width):
+            r,g,b = image.getpixel((x,y))
+            if r == 255 and g == 255 and b == 255:
+                maze[(x,y)] = " "
+            else:
+                maze[(x,y)] = "#"
+    frontier = [Node(COORDINATES[0][0],COORDINATES[0][1])]
+    explored = {}
+    explored[(COORDINATES[0][0],COORDINATES[0][1])] = Node(COORDINATES[0][0],COORDINATES[0][1])
+    while len(frontier) > 0:
+        current = frontier.pop()
+        if current.x == COORDINATES[1][0] and current.y == COORDINATES[1][1]:
+            break
+        neighbors = get_neighbors(current,maze)
+        for neighbor in neighbors:
+            if (neighbor.x,neighbor.y) in explored:
+                pass
+            else:
+                frontier.append(neighbor)
+                explored[(neighbor.x,neighbor.y)] = neighbor
+    path = []
+    while current.parent is not None:
+        path.append(current)
+        current = current.parent
+    path.reverse()
+    os.system("cls")
+    print("Path:")
+    for node in path:
+        print(node,end=" ")
+    print()
+    for node in path:
+        plt.plot(node.x,node.y,'b.', markersize=4)
+    fig.canvas.draw()
+    print("Nodes expanded:",len(explored))
+    print("Depth:",len(path))
+    print("Execution time:",time.time()-start)
+
+#Solving the maze with iterative deepening search
+def ids(max_depth):
+    start = time.time()
+    global X
+    global Y
+    maze = {}
+    image = Image.open(IMAGE_NAME)
+    image = image.convert('RGB')
+    X,Y = image.size[0]-1,image.size[1]-1
+    for y in range(image.height):
+        for x in range(image.width):
+            r,g,b = image.getpixel((x,y))
+            if r == 255 and g == 255 and b == 255:
+                maze[(x,y)] = " "
+            else:
+                maze[(x,y)] = "#"
+    frontier = [Node(COORDINATES[0][0],COORDINATES[0][1])]
+    explored = {}
+    explored[(COORDINATES[0][0],COORDINATES[0][1])] = Node(COORDINATES[0][0],COORDINATES[0][1])
+    while len(frontier) > 0:
+        current = frontier.pop()
+        if current.x == COORDINATES[1][0] and current.y == COORDINATES[1][1]:
+            break
+        neighbors = get_neighbors(current,maze)
+        for neighbor in neighbors:
+            if (neighbor.x,neighbor.y) in explored:
+                pass
+            else:
+                frontier.append(neighbor)
+                explored[(neighbor.x,neighbor.y)] = neighbor
+                if len(frontier) > max_depth:
+                    frontier.pop(0)
+    path = []
+    while current.parent is not None:
+        path.append(current)
+        current = current.parent
+    path.reverse()
+    
+    os.system("cls")
+    if len(path) > max_depth:
+        print("No solution found in",max_depth,"depth")
+        return
     print("Path:")
     for node in path:
         print(node,end=" ")
@@ -106,7 +206,7 @@ def onclick(event):
         COUNTER = [0,0]
         COORDINATES = [None,None]
     elif COUNTER[0] == 1 and COUNTER[1] == 1:
-        solve()
+        ids(30)
 
 fig, ax = plt.subplots()
 ax.set_title("Maze")
